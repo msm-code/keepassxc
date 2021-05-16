549cbefe9ba36b1af6bef1ff6bbd73ca129949c9
Xavier Valls <xaviervallspla@gmail.com>
2021-05-16 18:49:37 +0200

    update tag sidepanel after entry edit

--- src/gui/DatabaseWidget.cpp
+++ src/gui/DatabaseWidget.cpp
@@ -1459,6 +1459,7 @@ void DatabaseWidget::onEntryChanged(Entry* entry)
 {
     if (entry) {
         m_previewView->setEntry(entry);
+        static_cast<TagModel*>(m_tagView->model())->findTags();
     }
 
     emit entrySelectionChanged();
